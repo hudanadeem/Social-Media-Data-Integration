3250b2ee0b436062b236c62b14526969d6abf7bd
hnadee03 <hnadee03@uoguelph.ca>
2023-03-14 22:23:58 -0400

    cleaned up comments

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,14 @@
 import "./App.css";
 import React from 'react';
-// import Post from './Post';
-// import { getPosts } from "./api/api";
-// import Folder from './Folder';
-// import React, { useState, useEffect } from 'react';
-
-
-// import ReactDOM from 'react-dom'
 import Navbar from "./Components/Navbar";
 import {BrowserRouter as Router, Routes ,Route} from 'react-router-dom'
 import Home from './Components/pages/Home'
 
 
-function App() {
 
+function App() {
   return (
     <div className="app">
-      {/* <header className="app__header">
-        <h1>Weapons of Mass Destruction</h1>
-      </header> */}
       <Router>
         <Navbar />
           <Routes>
