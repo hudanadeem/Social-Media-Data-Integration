bdb4f413361259e6f865db6f12116e5070658752
Christopher Rupert <rupertc@uoguelph.ca>
2023-03-14 22:29:59 -0400

    Updated look of Created times

--- frontend/src/Post.js
+++ frontend/src/Post.js
@@ -1,13 +1,18 @@
 import React from 'react';
+import Moment from 'moment';
+import moment from 'moment';
 
 const Post = ({ post }) => {
-  const date = new Date(post.created * 1000);
-  const date_string = new Date(post.created * 1000).toLocaleDateString()
-  const hours = date.getHours();
-  const minutes = "0" + date.getMinutes();
-  const seconds = "0" + date.getSeconds();
-  const date_display = date_string + " " + hours + "/" + minutes.substr(-2) + "/" + seconds.substr(-2);
+  // const date = new Date(post.created * 1000);
+  // const date_string = new Date(post.created * 1000).toLocaleDateString()
+  // const hours = date.getHours();
+  // const minutes = "0" + date.getMinutes();
+  // const seconds = "0" + date.getSeconds();
+  // const date_display = date_string + " " + hours + "/" + minutes.substr(-2) + "/" + seconds.substr(-2);
   const ups = parseInt(post.ups).toLocaleString();
+
+  const date = moment.unix(post.created);
+  const date2 = date.format("MMMM Do YYYY, h:mm:ss a");
 
   return (
     <div className="post">
@@ -19,7 +24,7 @@ const Post = ({ post }) => {
         <p className="post-meta">Ups: {ups}</p>
         <p className="post-meta">Upvote Ratio: {post.upvote_ratio}</p>
         <p className="post-meta">Subreddit: {post.subreddit}</p>
-        <p className="post-meta">Created: {date_display}</p>
+        <p className="post-meta">Created: {date2}</p>
       </div>
     </div>
   );
